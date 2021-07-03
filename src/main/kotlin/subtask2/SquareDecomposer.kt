341a34749ccc9bd976a4688413b175399d08a92c
Sergey Ivanenko <storm06kr@gmail.com>
2021-07-04 02:03:37 +0300

    init: start subtask2

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -4,6 +4,7 @@ class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
+
         throw NotImplementedError("Not implemented")
     }
 }
