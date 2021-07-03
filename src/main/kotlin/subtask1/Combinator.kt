c03f42f6b2e667afcdf73be19182798bcc40996a
Sergey Ivanenko <storm06kr@gmail.com>
2021-07-03 23:33:55 +0300

    init: start subtask1

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,6 +4,7 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
+
         throw NotImplementedError("Not implemented")
     }
 }
