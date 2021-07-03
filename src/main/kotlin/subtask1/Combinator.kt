e733965de5f8d922081b8a28a4fc18ca3bbb7a99
Sergey Ivanenko <storm06kr@gmail.com>
2021-07-04 00:16:37 +0300

    feat: subtask1 has been done

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,7 +4,19 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
+        for (count in 1..array.last()) {
+            if (findNumberOfCombinations(array.last(), count) >= array.first()) {
+                return count
+            }
+        }
+        return null
+    }
 
-        throw NotImplementedError("Not implemented")
+    private fun factorial(number: Int): Long {
+        return if (number == 0 || number == 1) 1 else factorial(number - 1) * number
+    }
+
+    private fun findNumberOfCombinations(totalNumberOfColors: Int, numberOfColors: Int): Long {
+        return factorial(totalNumberOfColors) / (factorial(numberOfColors) * factorial(totalNumberOfColors - numberOfColors))
     }
 }
