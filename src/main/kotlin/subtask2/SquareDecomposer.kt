9efa7c9cfb9694e5e04e31df056253f13df3db01
Sergey Ivanenko <storm06kr@gmail.com>
2021-07-04 11:20:05 +0300

    feat: subtask2 has been done

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -4,7 +4,31 @@ class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
+        val decomposeArray = decomposer(number.toLong(), number * number.toLong()) ?: return null
+        decomposeArray.removeAt(decomposeArray.size - 1)
 
-        throw NotImplementedError("Not implemented")
+        return decomposeArray.toTypedArray()
+    }
+
+    private fun decomposer(number: Long, remain: Long): ArrayList<Int>?{
+        if(remain == 0L){
+            val list = ArrayList<Int>()
+            list.add(number.toInt())
+            return list
+        }
+
+        for(i in number - 1 downTo 1){
+            val currentDecNumber = remain - i * i
+            if(currentDecNumber >= 0){
+                val list = decomposer(i, currentDecNumber)
+
+                if(list != null){
+                    list.add(number.toInt());
+                    return list
+                }
+            }
+        }
+
+        return null;
     }
 }
